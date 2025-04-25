e07988d856b6a8e172d71c7de586d40619bd1496
RayTTPE <kantaphon.masu@bumail.net>
2025-04-25 12:52:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ from ติดต่อ.contact import contact_form
 import random
 import os
 from memory import get_prompt
-import docx
-import PyPDF2
 
 API_KEY = "AIzaSyDQ3dBumcz0BtrV9a6Zj68pl8N4C9_8b74"
 ollama_url = "https://cockatoo-loyal-rarely.ngrok-free.app/v1/chat/completions"
